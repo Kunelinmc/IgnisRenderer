0bf4c2bdd6eeb1a5150345ff075f771c16caf840
kunelinmc <kunelinmc@gmail.com>
2026-03-03 19:16:10 +0800

    feat: 還原更改

--- src/cameras/OrthographicCamera.ts
+++ src/cameras/OrthographicCamera.ts
@@ -5,11 +5,28 @@ import { Matrix4 } from "../maths/Matrix4";
  * An Orthographic Camera using a size-based orthographic projection.
  */
 export class OrthographicCamera extends Camera {
+	/** Provide the vertical size of the visible area. The horizontal size is computed via the aspect ratio. */
+	public size: number = 100;
+
 	constructor(size: number = 100) {
 		super();
 		this.type = CameraType.Orthographic;
 		this.size = size;
 
 		this.updateMatrices();
+	}
+
+	public override calculateProjectionMatrix(): Matrix4 {
+		const halfHeight = this.size / 2;
+		const halfWidth = halfHeight * this.aspectRatio;
+
+		return Matrix4.ortho(
+			-halfWidth,
+			halfWidth,
+			-halfHeight,
+			halfHeight,
+			this.near,
+			this.far
+		);
 	}
 }
