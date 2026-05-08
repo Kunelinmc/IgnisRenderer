4864fff8fac65768c2c624f921484902d06284c2
kunelinmc <kunelinmc@gmail.com>
2026-05-08 20:05:06 +0800

    feat: 新增Environment的全局導出

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ export * from "./foundation/Color";
 export * from "./foundation/Logger";
 export * from "./foundation/Platform";
 export { Node } from "./core/Node";
+export { Environment } from "./core/Environment";
 export { MeshAsset, MeshInstance, LODMeshInstance, MeshFactory } from "./meshes";
 export * from "./csg";
 export * from "./spatial";
