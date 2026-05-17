5d613577bfd9e47f8b360bb8d36e46a8103db320
kunelinmc <kunelinmc@gmail.com>
2026-05-17 21:48:05 +0800

    refactor: 移除未使用的方法

--- src/main.ts
+++ src/main.ts
@@ -11,46 +11,8 @@ import {
 	InteractionManager,
 	MeshInstance,
 	Logger,
-	AreaLight,
 	DirectionalLight,
 } from "./index";
-
-function colorTemperatureToRGB(kelvin) {
-	const temp = kelvin / 100;
-	let r, g, b;
-
-	// Red
-	if (temp <= 66) {
-		r = 255;
-	} else {
-		r = 329.698727446 * Math.pow(temp - 60, -0.1332047592);
-		r = Math.max(0, Math.min(255, r));
-	}
-
-	// Green
-	if (temp <= 66) {
-		g = 99.4708025861 * Math.log(temp) - 161.1195681661;
-	} else {
-		g = 288.1221695283 * Math.pow(temp - 60, -0.0755148492);
-	}
-	g = Math.max(0, Math.min(255, g));
-
-	// Blue
-	if (temp >= 66) {
-		b = 255;
-	} else if (temp <= 19) {
-		b = 0;
-	} else {
-		b = 138.5177312231 * Math.log(temp - 10) - 305.0447927307;
-		b = Math.max(0, Math.min(255, b));
-	}
-
-	return {
-		r: Math.round(r),
-		g: Math.round(g),
-		b: Math.round(b),
-	};
-}
 
 async function init() {
 	const platform = Platform.detect();
